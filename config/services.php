2f64e383b70ac57be7b7ec039d973646bc44a9f3
tdn283 <nhatflame@gmail.com>
2024-11-28 22:38:06 +0700

    feat(admin-crud): Added product images upload functionality

--- config/services.php
+++ config/services.php
@@ -41,4 +41,9 @@ return [
         'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
     ],
 
+    'cloudinary' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key' => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
 ];
